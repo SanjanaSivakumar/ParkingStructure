696947addf7fad5527affbcaa0ec483cf0e4a3df
SanjanaSivakumar <47901873+SanjanaSivakumar@users.noreply.github.com>
2019-09-08 21:29:04 -0500

    Create Motorcycle.h

---
+++ Motorcycle.h
@@ -0,0 +1,31 @@
+/*
+ * Motorcycle.h
+ *
+ *      Author: sanjana_mac
+ */
+//Sanjana Sivakumar CS2336.001
+//The purpose of this class is to set the definition for when a Bus object will be created.
+//This class is set up in such a way that aids in the functionality of the parking structure
+//This class is one of the derived classes of the Vehicle class
+
+#ifndef MOTORCYCLE_H_
+#define MOTORCYCLE_H_
+#include <iostream>
+#include "Vehicle.h"
+
+using namespace std;
+class Motorcycle: public Vehicle {
+private:
+	int size;
+	int position;
+public:
+	Motorcycle();
+	virtual ~Motorcycle();
+	int getSize();
+	string toString();
+	int getPosition();
+	void setPosition(int p);
+};
+
+
+#endif /* MOTORCYCLE_H_ */
